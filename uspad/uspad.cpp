4b8c9b72d4a1dacd97da8f6af9fc1da489819787
olegk <kbryzhenyukpc@gmail.com>
2024-10-25 17:19:11 +0300

    Добавьте файлы проекта.

---
+++ uspad/uspad.cpp
@@ -0,0 +1,23 @@
+﻿#include <cmath>
+
+class Position {
+public:
+    int x;
+    int y;
+
+    Position(int x = 0, int y = 0) : x(x), y(y) {}
+
+    int Distance(const Position& other) const {
+        int dx = x - other.x;
+        int dy = y - other.y;
+        return static_cast<int>(std::sqrt(dx * dx + dy * dy));
+    }
+
+    void MoveTowards(const Position& target) {
+        if (x < target.x) x++;
+        else if (x > target.x) x--;
+
+        if (y < target.y) y++;
+        else if (y > target.y) y--;
+    }
+};
